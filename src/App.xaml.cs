51872be0173250c45d41980db49ce4d49645a1a2
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 18:50:18 +0200

    Update App.xaml.cs

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,8 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 namespace TABFRET
 {
     public partial class App : Application
     {
+        public static ServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            // Register services and viewmodels
+            services.AddSingleton<IMidiParser, MidiParserDryWetMidi>();
+            services.AddSingleton<ISettingsService, SettingsService>();
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<FretboardViewModel>();
+            services.AddTransient<SettingsViewModel>();
+            services.AddTransient<PlaybackControlsViewModel>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            base.OnStartup(e);
+        }
     }
 }
