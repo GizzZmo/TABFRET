9957c6f4b1c6f041e1204a468ac67b71db84ede0
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 19:11:04 +0200

    Update UserSettings.cs

--- src/Models/UserSettings.cs
+++ src/Models/UserSettings.cs
@@ -5,7 +5,7 @@ namespace TABFRET.Models
     public class UserSettings
     {
         public int NumberOfStrings { get; set; } = 6;
-        public List<int> Tuning { get; set; } = new() { 64, 59, 55, 50, 45, 40 }; // EADGBE by default
+        public List<int> Tuning { get; set; } = new() { 64, 59, 55, 50, 45, 40 }; // EADGBE MIDI notes
         public string Theme { get; set; } = "Default";
     }
 }
