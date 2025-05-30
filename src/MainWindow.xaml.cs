4fda25fbc16ddb78d890b67f0e8915a987c5b3b6
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 16:16:49 +0200

    Create MainWindow.xaml.cs

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using TABFRET.ViewModels;
+
+namespace TABFRET
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // DataContext is set in XAML
+        }
+    }
+}
