93892cb780b3483e643c644bf006ea2c4dc86f61
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 16:31:11 +0200

    Update MainViewModel.cs

--- src/ViewModels/MainViewModel.cs
+++ src/ViewModels/MainViewModel.cs
@@ -1,19 +1,19 @@
 using System.Collections.ObjectModel;
 using TABFRET.Models;
 using TABFRET.Services;
+using TABFRET.Utils;
 using Microsoft.Win32;
 using System.Windows.Input;
 using System.ComponentModel;
+using System.Windows;
 
 namespace TABFRET.ViewModels
 {
-    /// <summary>
-    /// Main ViewModel: Handles MIDI parsing, tab mapping, and exposes data to the UI.
-    /// </summary>
     public class MainViewModel : INotifyPropertyChanged
     {
         private readonly MidiParser _midiParser = new MidiParser();
         private readonly TabMapper _tabMapper = new TabMapper();
+        private MidiPlaybackService _playbackService = new MidiPlaybackService();
 
         public ObservableCollection<TabNote> TabNotes { get; } = new ObservableCollection<TabNote>();
         public ObservableCollection<MidiNote> MidiNotes { get; } = new ObservableCollection<MidiNote>();
@@ -25,11 +25,29 @@ namespace TABFRET.ViewModels
             set { _currentFilePath = value; OnPropertyChanged(nameof(CurrentFilePath)); }
         }
 
+        private int _transposeSemitones = 0;
+        public int TransposeSemitones
+        {
+            get => _transposeSemitones;
+            set
+            {
+                _transposeSemitones = value;
+                OnPropertyChanged(nameof(TransposeSemitones));
+                UpdateTabNotes();
+            }
+        }
+
         public ICommand LoadMidiCommand { get; }
+        public ICommand PlaybackCommand { get; }
+        public ICommand TransposeUpCommand { get; }
+        public ICommand TransposeDownCommand { get; }
 
         public MainViewModel()
         {
             LoadMidiCommand = new RelayCommand(_ => LoadMidiFile());
+            PlaybackCommand = new RelayCommand(_ => PlayMidi());
+            TransposeUpCommand = new RelayCommand(_ => { TransposeSemitones++; });
+            TransposeDownCommand = new RelayCommand(_ => { TransposeSemitones--; });
         }
 
         public void LoadMidiFile()
@@ -42,20 +60,38 @@ namespace TABFRET.ViewModels
             if (dlg.ShowDialog() == true)
             {
                 CurrentFilePath = dlg.FileName;
-                _midiParser.LoadMidiFile(CurrentFilePath);
-
+                var loaded = _midiParser.LoadMidiFile(CurrentFilePath);
+                if (!loaded)
+                {
+                    MessageBox.Show(_midiParser.ErrorMessage, "MIDI Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
                 MidiNotes.Clear();
                 foreach (var note in _midiParser.Notes)
                     MidiNotes.Add(note);
-
-                var tabNotes = _tabMapper.MapMidiNotesToTab(_midiParser.Notes);
-                TabNotes.Clear();
-                foreach (var tabNote in tabNotes)
-                    TabNotes.Add(tabNote);
-
-                OnPropertyChanged(nameof(TabNotes));
-                OnPropertyChanged(nameof(MidiNotes));
+                UpdateTabNotes();
             }
+        }
+
+        private void UpdateTabNotes()
+        {
+            var notes = new List<MidiNote>(_midiParser.Notes);
+            if (TransposeSemitones != 0)
+            {
+                foreach (var note in notes)
+                    note.NoteNumber = FretboardHelper.Transpose(note.NoteNumber, TransposeSemitones);
+            }
+            var tabNotes = _tabMapper.MapMidiNotesToTab(notes);
+            TabNotes.Clear();
+            foreach (var tabNote in tabNotes)
+                TabNotes.Add(tabNote);
+
+            _playbackService.LoadMidiEvents(notes, _midiParser.TicksPerQuarterNote);
+        }
+
+        public void PlayMidi()
+        {
+            _playbackService.Play();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -63,7 +99,6 @@ namespace TABFRET.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 
-    // Simple ICommand implementation for binding
     public class RelayCommand : ICommand
     {
         private readonly System.Action<object> _execute;
