70bb62508ad207da7dcc03d8907a967ec00edb49
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 16:06:58 +0200

    Create MidiParser.cs

---
+++ src/Services/MidiParser.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using NAudio.Midi;
+using TABFRET.Models;
+
+namespace TABFRET.Services
+{
+    /// <summary>
+    /// Handles MIDI file parsing and exposes note events for tab mapping.
+    /// </summary>
+    public class MidiParser
+    {
+        public List<MidiNote> Notes { get; private set; } = new List<MidiNote>();
+        public int TicksPerQuarterNote { get; private set; } = 480; // Default, will be set by MIDI file
+
+        /// <summary>
+        /// Loads and parses a MIDI file.
+        /// </summary>
+        /// <param name="filePath">Path to the MIDI file.</param>
+        public void LoadMidiFile(string filePath)
+        {
+            Notes.Clear();
+
+            var midiFile = new MidiFile(filePath, false);
+            TicksPerQuarterNote = midiFile.DeltaTicksPerQuarterNote;
+
+            // Dictionary to keep track of note on events (key: note number, value: start time)
+            var noteOnTimes = new Dictionary<int, List<long>>();
+
+            long absoluteTime = 0;
+
+            foreach (var track in midiFile.Events)
+            {
+                absoluteTime = 0;
+                foreach (var midiEvent in track)
+                {
+                    absoluteTime += midiEvent.DeltaTime;
+
+                    if (midiEvent is NoteOnEvent noteOn && noteOn.Velocity > 0)
+                    {
+                        if (!noteOnTimes.ContainsKey(noteOn.NoteNumber))
+                            noteOnTimes[noteOn.NoteNumber] = new List<long>();
+                        noteOnTimes[noteOn.NoteNumber].Add(absoluteTime);
+                    }
+                    else if (midiEvent is NoteEvent noteEvent && noteEvent.CommandCode == MidiCommandCode.NoteOff)
+                    {
+                        // Note off, find matching note on
+                        if (noteOnTimes.ContainsKey(noteEvent.NoteNumber) && noteOnTimes[noteEvent.NoteNumber].Count > 0)
+                        {
+                            var startTime = noteOnTimes[noteEvent.NoteNumber][0];
+                            noteOnTimes[noteEvent.NoteNumber].RemoveAt(0);
+
+                            Notes.Add(new MidiNote
+                            {
+                                NoteNumber = noteEvent.NoteNumber,
+                                StartTimeTicks = startTime,
+                                DurationTicks = absoluteTime - startTime,
+                                Channel = noteEvent.Channel,
+                                Velocity = noteEvent.Velocity
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
