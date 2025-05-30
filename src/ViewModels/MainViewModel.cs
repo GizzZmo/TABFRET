7909cc4d13dda6bdd5e2da9e6de3ad591769791a
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 19:04:11 +0200

    Update MainViewModel.cs

--- src/ViewModels/MainViewModel.cs
+++ src/ViewModels/MainViewModel.cs
@@ -1,7 +1,6 @@
-namespace TABFRET.ViewModels
+namespace TABFRET.ViewModels;
     
 using System.Collections.ObjectModel;
-
 using System.Threading.Tasks;
 using System.Windows.Input;
 using TABFRET.Models;
