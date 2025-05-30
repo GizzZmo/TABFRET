ad0ae789e09806e551c4e28d4fc57b6864ac0b59
Jon Arve Ovesen <jonovesen@gmail.com>
2025-05-30 16:24:53 +0200

    Update MidiParser.cs

--- src/Services/MidiParser.cs
+++ src/Services/MidiParser.cs
@@ -5,63 +5,71 @@ using TABFRET.Models;
 
 namespace TABFRET.Services
 {
-    /// <summary>
-    /// Handles MIDI file parsing and exposes note events for tab mapping.
-    /// </summary>
     public class MidiParser
     {
         public List<MidiNote> Notes { get; private set; } = new List<MidiNote>();
-        public int TicksPerQuarterNote { get; private set; } = 480; // Default, will be set by MIDI file
+        public int TicksPerQuarterNote { get; private set; } = 480;
+        public string ErrorMessage { get; private set; }
 
-        /// <summary>
-        /// Loads and parses a MIDI file.
-        /// </summary>
-        /// <param name="filePath">Path to the MIDI file.</param>
-        public void LoadMidiFile(string filePath)
+        public bool LoadMidiFile(string filePath)
         {
             Notes.Clear();
+            ErrorMessage = null;
 
-            var midiFile = new MidiFile(filePath, false);
-            TicksPerQuarterNote = midiFile.DeltaTicksPerQuarterNote;
-
-            // Dictionary to keep track of note on events (key: note number, value: start time)
-            var noteOnTimes = new Dictionary<int, List<long>>();
-
-            long absoluteTime = 0;
-
-            foreach (var track in midiFile.Events)
+            try
             {
-                absoluteTime = 0;
-                foreach (var midiEvent in track)
+                var midiFile = new MidiFile(filePath, false);
+                TicksPerQuarterNote = midiFile.DeltaTicksPerQuarterNote;
+
+                var noteOnTimes = new Dictionary<int, List<long>>();
+                long absoluteTime = 0;
+
+                foreach (var track in midiFile.Events)
                 {
-                    absoluteTime += midiEvent.DeltaTime;
+                    absoluteTime = 0;
+                    foreach (var midiEvent in track)
+                    {
+                        absoluteTime += midiEvent.DeltaTime;
 
-                    if (midiEvent is NoteOnEvent noteOn && noteOn.Velocity > 0)
-                    {
-                        if (!noteOnTimes.ContainsKey(noteOn.NoteNumber))
-                            noteOnTimes[noteOn.NoteNumber] = new List<long>();
-                        noteOnTimes[noteOn.NoteNumber].Add(absoluteTime);
-                    }
-                    else if (midiEvent is NoteEvent noteEvent && noteEvent.CommandCode == MidiCommandCode.NoteOff)
-                    {
-                        // Note off, find matching note on
-                        if (noteOnTimes.ContainsKey(noteEvent.NoteNumber) && noteOnTimes[noteEvent.NoteNumber].Count > 0)
+                        if (midiEvent is NoteOnEvent noteOn && noteOn.Velocity > 0)
                         {
-                            var startTime = noteOnTimes[noteEvent.NoteNumber][0];
-                            noteOnTimes[noteEvent.NoteNumber].RemoveAt(0);
-
-                            Notes.Add(new MidiNote
+                            if (!noteOnTimes.ContainsKey(noteOn.NoteNumber))
+                                noteOnTimes[noteOn.NoteNumber] = new List<long>();
+                            noteOnTimes[noteOn.NoteNumber].Add(absoluteTime);
+                        }
+                        else if (midiEvent is NoteEvent noteEvent && noteEvent.CommandCode == MidiCommandCode.NoteOff)
+                        {
+                            if (noteOnTimes.ContainsKey(noteEvent.NoteNumber) && noteOnTimes[noteEvent.NoteNumber].Count > 0)
                             {
-                                NoteNumber = noteEvent.NoteNumber,
-                                StartTimeTicks = startTime,
-                                DurationTicks = absoluteTime - startTime,
-                                Channel = noteEvent.Channel,
-                                Velocity = noteEvent.Velocity
-                            });
+                                var startTime = noteOnTimes[noteEvent.NoteNumber][0];
+                                noteOnTimes[noteEvent.NoteNumber].RemoveAt(0);
+
+                                Notes.Add(new MidiNote
+                                {
+                                    NoteNumber = noteEvent.NoteNumber,
+                                    StartTimeTicks = startTime,
+                                    DurationTicks = absoluteTime - startTime,
+                                    Channel = noteEvent.Channel,
+                                    Velocity = noteEvent.Velocity
+                                });
+                            }
                         }
                     }
                 }
+                return true;
             }
+            catch (Exception ex)
+            {
+                ErrorMessage = "Failed to load MIDI: " + ex.Message;
+                return false;
+            }
+        }
+
+        // Transpose all loaded notes by N semitones
+        public void TransposeNotes(int semitones)
+        {
+            foreach (var note in Notes)
+                note.NoteNumber += semitones;
         }
     }
 }
